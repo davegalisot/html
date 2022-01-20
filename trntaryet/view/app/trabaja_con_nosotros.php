e29a1bb8e8c52cb0ae8582871179d0e78fd6126b
Davegalisot <davegalisot@gmail.com>
2022-01-20 22:31:09 +0100

    creo la página trabaja con nosotros

---
+++ trntaryet/view/app/trabaja_con_nosotros.php
@@ -0,0 +1,38 @@
+        <section class="mt-5 mx-auto">
+            <article class="article-titulo">
+                <div class="foto-titulo">
+                    <h1 class="text-center tcnh1 animated slideInUp faster">trabaja con nosotros</h1>
+                    <div class="blanco-tapa-h1"></div>
+                </div>
+            </article>
+            <article class="width1500px mx-auto">                
+            </article>
+            <article>
+                <div class="texto-cualidades fondo-cualidades text-center">
+                    <p>TRN TÁRYET se apoya en un equipo técnico con cuatro cualidades básicas</p>
+                    <p>que lo distinguen, constituyendo una oferta diferenciada en el mercado:</p>
+                </div>
+                <div class="row div-cualidades width1500px mx-auto text-center">
+                    <div class="col-xl-3 col-lg-3 col-6">
+                        <i class="fas fa-microscope"></i>
+                        <p>Sólida formación</p>
+                        <p>científica</p>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-6">
+                        <i class="fas fa-drafting-compass"></i>
+                        <p>Amplia experiencia</p>
+                        <p>técnica</p>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-6">
+                        <i class="fas fa-laptop-code"></i>
+                        <p>Software de apoyo</p>
+                        <p>altamente especializado</p>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-6">
+                        <i class="fas fa-award"></i>
+                        <p>Especial dedicación y</p>
+                        <p>entrega a cada trabajo</p>
+                    </div>
+                </div>
+            </article>
+        </section>
